fbe6dd2f9a55c9460eba2e89f11a57f44778954c
[seraph] <[hw_developer@163.com]>
2017-12-26 17:46:41 +0800

    添加代码

---
+++ 09mysql/03查询/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var UserDao = require('./UserDao');
+var app = express();
+
+var bodyParser = require('body-parser');
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+
+    var dao = new UserDao();
+
+    dao.init();
+
+    dao.query(0,'users',function (err, data) {
+        
+        console.log(data);
+       
+    })
+
+
+})
+var server = app.listen(8088)
