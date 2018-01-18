5d2d8bbbae3e7683e8814e1887ee69bd14944f9c
[seraph] <[hw_developer@163.com]>
2018-01-18 14:04:47 +0800

    ejs 包含文件  并且传递参数

---
+++ 03ejs模板引擎/02 ejs/01http.js
@@ -0,0 +1,34 @@
+/**
+ * Created by boy on 2017/7/10.
+ */
+//1,引入express模块
+var express = require("express");
+//2,创建app对象
+var app = express();
+//3,指定模板引擎
+app.set("views engine", 'ejs');
+//4,指定模板位置
+app.set('views', __dirname + '/views');
+
+//5,路由
+app.get("/", function(req, res) {
+    //6,利用模板文件home.ejs渲染为html
+    var Student = require('./been/Student.js');
+    var length=5;
+    var students = new Array();
+    for(var i=0;i<length;i++){
+        var student = new Student('小明'+i,19+i);
+        students[i]=student;
+    }
+
+    res.render('home', {
+        introduce:'头文件的信息',
+            order:{price:'100'},
+        name: '门户页面',users:students
+    }
+    );
+});
+
+var server = app.listen(8088, function() {
+    console.log("请在浏览器访问：http://localhost:8088/");
+});
